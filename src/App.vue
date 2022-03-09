f375bda2d0ccfe3e2003c8602e8dc3e842cae984
Wolfram Zell <w.zell@teamorange.de>
2022-03-09 16:23:13 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main_container">
+    <Loading/>
+  </div>
+</template>
+
+<script lang="ts">
+import Loading from "@/components/Loading.vue";
+
+export default {
+  components: {
+    Loading,
+  },
+};
+</script>
+
+<style lang="scss">
+  .main_container {
+    background-color: #fafafa;
+    display: grid;
+    min-height: 100vh;
+    place-content: center;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="loading_wrapper">
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Loading"
+};
+</script>
+
+<style lang="scss" scoped>
+  .loading_wrapper {
+    background-color: white;
+    border: 1px solid #efefef;
+    height: 30rem;
+    width: 30rem;
+  }
+</style>
